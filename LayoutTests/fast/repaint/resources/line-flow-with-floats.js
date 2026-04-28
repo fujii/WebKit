a7b2ad4d45506b020696146c9c73c8c31be11d00
Cameron McCormack <heycam@apple.com>
2026-04-28 15:36:33 -0700

    Make line-flow-with-float-* layout tests pass in Site Isolation mode https://bugs.webkit.org/show_bug.cgi?id=313515 rdar://problem/175726707
    Reviewed by Ryosuke Niwa.
    
    * LayoutTests/fast/repaint/resources/line-flow-with-floats.js:
    (async loaded):
    
    The true fix here is returning early when loadCount != 2. Before, we
    were continuing to call notifyDone even when loadCount == 1.
    
    (async beginTest):
    
    Simplify the test, merging this up into loaded.
    
    Note that while this test is in fast/repaint/, and had a
    testRunner.displayAndTrackRepaints() call, we weren't actually doing
    anything with the tracked repaints, so it wasn't acting as a repaint
    test at all.
    
    * LayoutTests/platform/mac-site-isolation/TestExpectations:
    
    Tests now pass.
    
    * LayoutTests/platform/mac/fast/repaint/line-flow-with-floats-1-expected.png: Removed.
    * LayoutTests/platform/mac/fast/repaint/line-flow-with-floats-10-expected.png: Removed.
    * LayoutTests/platform/mac/fast/repaint/line-flow-with-floats-2-expected.png: Removed.
    * LayoutTests/platform/mac/fast/repaint/line-flow-with-floats-3-expected.png: Removed.
    * LayoutTests/platform/mac/fast/repaint/line-flow-with-floats-4-expected.png: Removed.
    * LayoutTests/platform/mac/fast/repaint/line-flow-with-floats-5-expected.png: Removed.
    * LayoutTests/platform/mac/fast/repaint/line-flow-with-floats-6-expected.png: Removed.
    * LayoutTests/platform/mac/fast/repaint/line-flow-with-floats-7-expected.png: Removed.
    * LayoutTests/platform/mac/fast/repaint/line-flow-with-floats-8-expected.png: Removed.
    * LayoutTests/platform/mac/fast/repaint/line-flow-with-floats-9-expected.png: Removed.
    
    Removed these since the text comparison is all we need.
    
    Canonical link: https://commits.webkit.org/312241@main

--- LayoutTests/fast/repaint/resources/line-flow-with-floats.js
+++ LayoutTests/fast/repaint/resources/line-flow-with-floats.js
@@ -1,27 +1,11 @@
-var frameDoc;
 var loadCount = 0;
 
 async function loaded()
 {
     window.testRunner?.waitUntilDone();
-    loadCount++;
-    if (loadCount == 2) {
-        document.body.offsetTop;
-        await beginTest();
-    }
+    if (++loadCount < 2)
+        return;
+    document.body.offsetTop;
+    test(iframe.contentDocument);
     window.testRunner?.notifyDone();
-}
-
-async function beginTest()
-{
-    if (window.testRunner) {
-        document.body.offsetTop;
-        await testRunner.displayAndTrackRepaints();
-        test(document.getElementById("iframe").contentDocument);
-    } else setTimeout(
-        function() {
-            test(document.getElementById("iframe").contentDocument);
-        },
-        10
-    );
 }
