e9f59c8ee6e5cd7acf94b218b6e7ce93d6530a5a
Andrew Fryer <a_fryer@apple.com>
2026-05-13 06:48:45 -0700

    Disallow quick tier up recovery for builtins https://bugs.webkit.org/show_bug.cgi?id=314563 rdar://176411718
    Reviewed by Yusuke Suzuki and Keith Miller.
    
    In 309230@main, we allowed tier up to recover. Since js builtins are
    used across many workloads, we already don't save their value profiles
    to UnlinkedCodeBlock. Now, don't allow quick tier up to recover for js
    builtins. Also, change m_quickFTLTierUp from bool to TriState so that we
    can prevent recovery for FTL.
    
    No new behaviour.
    * Source/JavaScriptCore/bytecode/CodeBlock.cpp:
    (JSC::CodeBlock::didInstallDFGCode):
    (JSC::CodeBlock::didInstallFTLCode):
    (JSC::CodeBlock::didFTLJettison):
    (JSC::CodeBlock::didFailFTLCompilation):
    * Source/JavaScriptCore/bytecode/UnlinkedCodeBlock.h:
    (JSC::UnlinkedCodeBlock::hasQuickFTLTierUpUpdated const):
    (JSC::UnlinkedCodeBlock::isQuickFTLTierUp const):
    (JSC::UnlinkedCodeBlock::setQuickFTLTierUp):
    
    Canonical link: https://commits.webkit.org/313155@main

--- Source/JavaScriptCore/bytecode/UnlinkedCodeBlock.h
+++ Source/JavaScriptCore/bytecode/UnlinkedCodeBlock.h
@@ -181,8 +181,9 @@ public:
     void setQuickDFGTierUp(TriState state) { m_quickDFGTierUp = state; }
     TriState quickDFGTierUp() const { return m_quickDFGTierUp; }
 
-    bool isQuickFTLTierUp() const { return m_quickFTLTierUp; }
-    void setQuickFTLTierUp(bool value) { m_quickFTLTierUp = value; }
+    bool hasQuickFTLTierUpUpdated() const { return m_quickFTLTierUp != TriState::Indeterminate; }
+    bool isQuickFTLTierUp() const { return m_quickFTLTierUp == TriState::True; }
+    void setQuickFTLTierUp(TriState state) { m_quickFTLTierUp = state; }
 
     // Special registers
     void setThisRegister(VirtualRegister thisRegister) { m_thisRegister = thisRegister; }
@@ -440,7 +441,7 @@ private:
     bool m_hasCheckpoints : 1;
     LexicallyScopedFeatures m_lexicallyScopedFeatures : bitWidthOfLexicallyScopedFeatures { 0 };
     TriState m_quickDFGTierUp : 2 { TriState::Indeterminate };
-    bool m_quickFTLTierUp : 1 { false };
+    TriState m_quickFTLTierUp : 2 { TriState::Indeterminate };
 
 public:
     ConcurrentJSLock m_lock;
